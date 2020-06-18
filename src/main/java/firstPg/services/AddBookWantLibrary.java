44a33b302f2fc83972ac0a99899f030aed9d6595
PAP DENISA <papdeni11@gmail.com>
2020-06-18 17:38:49 +0300

    RT-14 changed pages design and layout

--- src/main/java/firstPg/services/AddBookWantLibrary.java
+++ src/main/java/firstPg/services/AddBookWantLibrary.java
@@ -18,17 +18,16 @@ public class AddBookWantLibrary extends JFrame {
 
     public AddBookWantLibrary(User user) throws FileNotFoundException {
 
-        this.user=user;
-        JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setTitle("Want to Read Library");
-        frame.setResizable(false);
-        frame.setSize(500, 600);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        this.user = user;
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("Want to Read Library");
+        setResizable(false);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
 
-        frame.addWindowListener(new WindowAdapter() {
+        this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 ReaderView view = new ReaderView(user);
@@ -37,16 +36,24 @@ public class AddBookWantLibrary extends JFrame {
             }
         });
 
+        Container contentPane = this.getContentPane();
+        contentPane.setLayout(null);
+        Color c = Color.pink;
+        contentPane.setBackground(c);
+        Color cl = Color.lightGray;
+
+        JPanel panel1 = new JPanel();// top panel
+
+        panel1.setBounds(0,15,500, 600);
+        panel1.setBackground(c);
+        contentPane.add(panel1);
+
         JTable table = new JTable();
-
         String readLine = null;
-
         BooksListTableModel tableModel = new BooksListTableModel();
         File file = new File("src/main/resources/WantToReadLibrary");
-
         FileReader reader = new FileReader(file);
         BufferedReader bufReader = new BufferedReader(reader);
-
         List<Books> booksList = new ArrayList<>();
 
         try {
@@ -68,14 +75,10 @@ public class AddBookWantLibrary extends JFrame {
         table.setModel(tableModel);
 
         Color pink=new Color(255, 230, 235, 255);
-        Color pink_d=new Color(255, 182, 199, 211); // 255,182,193
-
         table.setBackground(pink);
-        table.getTableHeader().setBackground(pink_d);
-
+        table.getTableHeader().setBackground(c);
         JScrollPane scroll = new JScrollPane(table);
-        scroll.getViewport().setBackground(pink_d);
-        frame.add(scroll);
-
+        scroll.getViewport().setBackground(pink);
+        panel1.add(scroll);
     }
 }
