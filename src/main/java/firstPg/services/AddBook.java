d8ebc4bb558fca1db6aa7f44074de58f0fea98b3
PAP DENISA <papdeni11@gmail.com>
2020-06-20 22:51:59 +0300

    remove popup when testing

--- src/main/java/firstPg/services/AddBook.java
+++ src/main/java/firstPg/services/AddBook.java
@@ -24,7 +24,7 @@ public class AddBook extends JFrame {
             reader1.write(Data);
             reader1.newLine();
             reader1.close();
-            JOptionPane.showMessageDialog(null, "Book added!", "Adding book", JOptionPane.INFORMATION_MESSAGE);
+            //JOptionPane.showMessageDialog(null, "Book added!", "Adding book", JOptionPane.INFORMATION_MESSAGE);
             dispose();
         } catch (IOException E) {
             System.out.println("Error is " + E);
